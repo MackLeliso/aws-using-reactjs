bfb4f535f60755a94b98eed15e9130769a253f98
leliso <lelisoagegnehu491@gmail.com>
2022-08-14 16:52:04 +0300

    add product data

---
+++ src/Utils/productData.js
@@ -0,0 +1,26 @@
+const ProductData = {
+  title: "Zombie ipsum reversus ab viral inferno, nam rick grimes malum ",
+  description:
+    "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum",
+  colorOptions: [
+    {
+      styleName: "Black Strap",
+      imageUrl: "https://imgur.com/iOeUBV7.png",
+    },
+    {
+      styleName: "Red Strap",
+      imageUrl: "https://imgur.com/PTgQlim.png",
+    },
+    {
+      styleName: "Blue Strap",
+      imageUrl: "https://imgur.com/Mplj1YR.png",
+    },
+    {
+      styleName: "Purpl Strap",
+      imageUrl: "https://imgur.com/xSIK4M8.png",
+    },
+  ],
+  featureList: ["Time", "Heart Rate"]
+};
+
+module.exports = {ProductData}
